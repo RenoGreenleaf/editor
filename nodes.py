b83daeb9f8910f2541d59e0885270e95d02666ec
reno.greenleaf@gmail.com <Reno Greenleaf>
2025-12-26 13:10:41 +0300

    Captions.

--- nodes.py
+++ nodes.py
@@ -1,16 +1,21 @@
 import qtpynodeeditor as ne
 
 
-class Data(ne.NodeData):
-	data_type = ne.NodeDataType('data', 'Data')
+class Boolean(ne.NodeData):
+	data_type = ne.NodeDataType('boolean', 'Boolean')
 
 
 class Option(ne.NodeDataModel):
 	name = 'option'
+	port_caption_visible = True
+	port_caption = {
+		'input': {0: 'Hide', 1: 'Show'},
+		'output': {0: 'When selected'}
+	}
 	caption = 'Option'
 	caption_visible = True
 	num_ports = {
 		ne.PortType.input: 2,
 		ne.PortType.output: 1,
 	}
-	data_type = Data.data_type
+	data_type = Boolean.data_type
